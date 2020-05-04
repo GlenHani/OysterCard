e7d9eef401f96ebffae90ffea65f584a9f8900e9
Cassis <cassismjones@gmail.com>
2020-05-04 14:52:21 +0100

    Section 5

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,11 @@
 class OysterCard
     attr_accessor :balance
-    
+
     def initialize (balance = 0)
-        @balance = balance
-      end
-    
-end
+      @balance = balance
+    end
+
+    def top_up(amount)
+      @balance = balance + amount
+    end
+  end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,8 @@ describe OysterCard do
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
+
+  it 'can top up the balance' do
+    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+  end
 end
